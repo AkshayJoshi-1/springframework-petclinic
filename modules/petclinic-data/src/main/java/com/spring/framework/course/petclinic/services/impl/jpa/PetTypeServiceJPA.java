6fe3ab8a0f5dadd4244c8c01bf905eb200be9511
David Webb <error1209@hotmail.com>
2020-07-12 15:54:33 +0530

    - Added PetType Service with JPA repositories

---
+++ modules/petclinic-data/src/main/java/com/spring/framework/course/petclinic/services/impl/jpa/PetTypeServiceJPA.java
@@ -0,0 +1,49 @@
+package com.spring.framework.course.petclinic.services.impl.jpa;
+
+import com.spring.framework.course.petclinic.model.PetType;
+import com.spring.framework.course.petclinic.repository.PetTypeRepository;
+import com.spring.framework.course.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetTypeServiceJPA implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceJPA(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> result = new HashSet<>();
+        petTypeRepository.findAll().forEach(result::add);
+        return result;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
