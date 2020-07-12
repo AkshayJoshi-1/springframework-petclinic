96c247768d4c52f1692417699b42ff358bbdb42e
David Webb <error1209@hotmail.com>
2020-07-12 16:22:18 +0530

    - Added Visit service with Map Implementation

---
+++ modules/petclinic-data/src/main/java/com/spring/framework/course/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.spring.framework.course.petclinic.services;
+
+import com.spring.framework.course.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
